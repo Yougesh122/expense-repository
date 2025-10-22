973b8ed3e03ba8995c7f192d3e3e8158e3b8eacf
Yougesh122 <142184827+Yougesh122@users.noreply.github.com>
2025-10-22 15:58:12 +0400

    Update api.php

--- Modules/Expense/routes/api.php
+++ Modules/Expense/routes/api.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Expense\Http\Controllers\Api\ExpenseController;
 
-// Route::prefix('v1')->group(function () {
-//     Route::apiResource('expenses', ExpenseController::class)->names('expense');
-// });
-
 Route::prefix('v1/expenses')->group(function () {
     Route::get('/{id}', [ExpenseController::class, 'show']);
     Route::get('/', [ExpenseController::class, 'index']);
